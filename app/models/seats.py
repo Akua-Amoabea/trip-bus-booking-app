64f2870a2a8f13c756d753d01f5d8bdda3b0982e
Akua Amoabea <150204296+Akua-Amoabea@users.noreply.github.com>
2026-08-18 12:38:06 +0000

    add seats model

--- app/models/seats.py
+++ app/models/seats.py
@@ -6,11 +6,11 @@ from sqlalchemy import Column, String, Boolean, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 
 
-class Route(Base):
+class Seats(Base):
     __tablename__ = "seats"
     
     uuid=Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     seat_number=Column(String, nullable=False)
     is_available=Column(Boolean,nullable=False)
     route_uuid=Column(UUID(as_uuid=True), ForeignKey=("routes.uuid"), nullable=False)
-     
+    bus_uuid=Column(UUID(as_uuid=True), ForeignKey=("buses.uuid"), nullable=False)
